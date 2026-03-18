d2efead40ce9e690febd2afe408b816702406f89
Srppa <pavelsrp1998@gmail.com>
2026-03-18 19:27:15 +0100

    spider

---
+++ Assets/Model/Blackwidow/Blackwidow_readme.txt
@@ -0,0 +1,37 @@
+Black widow 3D model.
+
+Root Motion
+8 Animations
+2 Textures (4096x4096)
+1 Prefab
+
+Low poly model
+1726 quad polygons on this mesh
+1728 quad polygons on this mesh
+
+Textures
+blackwidow_D.png
+blackwidow_N.png
+
+Animations List
+0-24 blackwidow_walk
+27-35 blackwidow_run
+38-68 blackwidow_turnleft
+71-101 blackwidow_turnright
+104-152 blackwidow_idle
+153-174 blackwidow_attack
+177-192 blackwidow_hit
+195-213 blackwidow_die
+
+Scene Demo
+keys for enable animations
+
+key 1=Idle
+key 2=attack
+key 3=die
+arrow up=run
+arrow down=walk
+arrow left=turn_left
+arrow right=turn_right
+
+enjoy:)
---
+++ Assets/Model/Blackwidow/Scripts/Blackwidow.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blackwidow : MonoBehaviour {
+    Animator blackwidow;
+    private IEnumerator coroutine;
+	// Use this for initialization
+	void Start () {
+        blackwidow = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            blackwidow.SetBool("idle", true);
+            blackwidow.SetBool("walk", false);
+            blackwidow.SetBool("run", false);
+            blackwidow.SetBool("hit", false);
+            blackwidow.SetBool("attack", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            blackwidow.SetBool("idle", false);
+            blackwidow.SetBool("attack", true);
+            blackwidow.SetBool("run", false);
+            blackwidow.SetBool("walk", false);
+            blackwidow.SetBool("hit", false);
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            blackwidow.SetBool("idle", false);
+            blackwidow.SetBool("hit", true);
+            blackwidow.SetBool("walk", false);
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            blackwidow.SetBool("idle", false);
+            blackwidow.SetBool("die", true);
+        }
+        if (Input.GetKey("down"))
+        {
+            blackwidow.SetBool("walk", true);
+            blackwidow.SetBool("idle", false);
+            blackwidow.SetBool("run", false);
+            blackwidow.SetBool("hit", false);
+        }
+        if (Input.GetKey("up"))
+        {
+            blackwidow.SetBool("run", true);
+            blackwidow.SetBool("walk", false);
+        }
+        if (Input.GetKey("left"))
+        {
+            blackwidow.SetBool("turn_left", true);
+            blackwidow.SetBool("run", false);
+            blackwidow.SetBool("walk", false);
+            StartCoroutine("walk");
+            walk();
+        }
+        if (Input.GetKey("right"))
+        {
+            blackwidow.SetBool("turn_right", true);
+            blackwidow.SetBool("run", false);
+            blackwidow.SetBool("walk", false);
+            StartCoroutine("walk");
+            walk();
+        }
+    }
+    IEnumerator walk()
+    {
+        yield return new WaitForSeconds(0.7f);
+        blackwidow.SetBool("turn_left", false);
+        blackwidow.SetBool("turn_right", false);
+        blackwidow.SetBool("walk", true);
+    }
+
+    IEnumerator idle()
+    {
+        yield return new WaitForSeconds(0.5f);
+        blackwidow.SetBool("idle", true);
+        blackwidow.SetBool("hit", false);
+        blackwidow.SetBool("attack", false);
+    }
+}
